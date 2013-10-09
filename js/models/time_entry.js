368131bcfde7c7673fd8872787e3e62e1837c57f
David Strauß <mail@stravid.com>
2013-10-09 11:53:02 +0200

    Introduce duration property which shields outer world of it's logic

--- js/models/time_entry.js
+++ js/models/time_entry.js
@@ -3,32 +3,49 @@ App.TimeEntry = DS.Model.extend({
   endedAt: DS.attr('number'),
   task: DS.belongsTo('task'),
   intervalId: null,
-  secondsSinceStartedAt: null,
+  virtualEndedAt: null,
+
+  duration: function() {
+    var startedAt = this.get('startedAt');
+    var endedAt = this.get('endedAt');
+    var virtualEndedAt = this.get('virtualEndedAt');
+
+    if (startedAt > 0 && endedAt > 0) {
+      return endedAt - startedAt;
+    }
+
+    if (startedAt > 0 && virtualEndedAt > 0) {
+      return virtualEndedAt - startedAt;
+    }
+
+    return 0;
+  }.property('startedAt', 'endedAt', 'virtualEndedAt'),
+
   startedAtChanged: function() {
     if (this.get('startedAt') > 0 && !this.get('endedAt') && !this.get('intervalId')) {
       this.startInterval();
     }
   }.observes('startedAt'),
+
   endedAtChanged: function() {
     if (this.get('endedAt') > 0) {
       this.clearInterval();
     }
   },
+
   startInterval: function() {
     var self = this;
     var intervalId = setInterval(function() {
-      var startedAt = self.get('startedAt');
-
-      if (startedAt) {
-        self.set('secondsSinceStartedAt', Math.floor((new Date().getTime() - startedAt) / 1000));
-      }
+      self.set('virtualEndedAt', new Date().getTime());
     }, 1000);
 
     this.set('intervalId', intervalId);
   },
+
   stopInterval: function() {
     clearInterval(this.get('intervalId'));
   },
+
   willDestroy: function() {
     this.stopInterval();
     this._super();
