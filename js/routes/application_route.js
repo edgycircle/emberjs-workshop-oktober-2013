7217d8d75f99ad9578b4918e10fef67706f2ee71
David Strauß <mail@stravid.com>
2013-10-17 18:24:59 +0200

    Re-add check for already active time entry

--- js/routes/application_route.js
+++ js/routes/application_route.js
@@ -4,9 +4,9 @@ App.ApplicationRoute = Ember.Route.extend({
     this.controllerFor('sidebar').set('projects', this.store.find('project'));
     this.controllerFor('sidebar').set('timeEntries', this.store.find('timeEntry'));
 
-    // var self = this;
-    // App.TimeEntry.findActive().then(function(timeEntry) {
-    //   self.controllerFor('sidebar').set('activeTimeEntry', timeEntry);
-    // });
+    var self = this;
+    App.TimeEntry.findActive().then(function(timeEntry) {
+      self.controllerFor('sidebar').set('activeTimeEntry', timeEntry);
+    });
   }
 });
