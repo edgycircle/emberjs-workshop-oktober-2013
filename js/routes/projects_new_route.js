0b0774d45b807ff198f3011b1df4ff164dec65f2
Thomas Buchöster <thomas.buchoester@gmail.com>
2013-10-18 14:25:08 +0200

    Deletes project if creation wasn´t completed

--- js/routes/projects_new_route.js
+++ js/routes/projects_new_route.js
@@ -2,6 +2,11 @@ App.ProjectsNewRoute = Ember.Route.extend({
   model: function() {
     return this.store.createRecord('project');
   },
+  deactivate: function() {
+    if (this.currentModel.get('isNew')) {
+      this.currentModel.deleteRecord()
+    }
+  },
   actions: {
     createProject: function() {
       var self = this;
